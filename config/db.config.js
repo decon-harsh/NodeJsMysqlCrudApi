8abdfdcbbe3c200c426d4251a882a96c9696dc1c
decon-harsh <harshsingh9471@gmail.com>
2021-01-21 00:50:08 +0530

    Connections

--- config/db.config.js
+++ config/db.config.js
@@ -2,11 +2,13 @@
 const mysql = require('mysql');
 
 const dbConn = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'root',
-  password : 'samsung',
-  database : 'node_mysql_crud_db'
+  host     : 'mysql80-afe9.euw2.cloud.ametnes.com',
+  port     : '3316',
+  user     : 'SptzXXJsFr',
+  database : '7750202550',
+  password : 'FjyeELGT5DoASDVZGsEo'
 });
+
 dbConn.connect(function(err) {
   if (err) throw err;
   console.log("Database Connected!");
